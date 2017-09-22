3c7f81c5549e447871c46bcb00d0c894aa66d24e
Amr Gharseldin <gharseldin@aucegypt.edu>
2017-09-21 21:21:46 -0700

    Trees -Added height and depth to AbstractTree

--- src/main/trees/AbstractTree.java
+++ src/main/trees/AbstractTree.java
@@ -19,4 +19,18 @@ public abstract class AbstractTree<E> implements Tree<E>{
 	public boolean isEmpty(){
 		return size() == 0;
 	}
+	
+	public int depth(Position<E> p){
+		if(isRoot(p))
+			return 0;
+		else
+			return 1+ depth(parent(p));
+	}
+	
+	public int height(Position<E> p){
+		int h = 0;
+		for(Position<E> c: children(p))
+			h= Math.max(h,  1+height(c));
+		return h;
+	}
 }
