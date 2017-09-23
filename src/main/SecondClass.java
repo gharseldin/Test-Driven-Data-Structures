992e1d2b1e983049401b351393fca2baabec2ffc
Amr Gharseldin <gharseldin@aucegypt.edu>
2017-09-22 23:07:57 -0700

    Regex back ref testing

--- src/main/SecondClass.java
+++ src/main/SecondClass.java
@@ -1,3 +1,5 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SecondClass {
 
@@ -5,5 +7,13 @@ public class SecondClass {
 	
 	public void decrement() {
 		variable--;
+	}
+	
+	public static void main(String[] args){
+		Pattern pattern = Pattern.compile("g1.*(g2).*(g3)");
+		Matcher matcher = pattern.matcher("hg1hg2hg3hg4h2");
+		
+		
+		System.out.println(matcher.replaceAll("-- $2 --- $1-- "));
 	}
 }
